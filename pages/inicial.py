9873c6d4a037d4521f01c28080f170caf49db6bd
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-05-31 16:47:09 -0400

    Update inicial.py

--- pages/inicial.py
+++ pages/inicial.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.set_page_config("Primeiros passos", layout="wide")
-st.markdown("Aqui vamos ter os link mais importantes da historia do brasil")
-st.image("bob.jpg")
+st.markdown("Este aqui é Flocos. Foi adotado e devolvido posteriormente")
+st.image("flocos.jpg")
 
