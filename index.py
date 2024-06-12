b9e9e5a77c5b11d8911b8903ed3a731f3b87c142
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-12 16:44:19 -0300

    Atualizar o index.py

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.markdown("PAGINAL INICIAL de ,uitas outrsa")
+st.markdown("Sistema Ágil")
 st.write("---")
-st.image("bob.jpg")
+st.image("https://www.google.com/search?client=ms-android-americamovil-br-rvc3&sca_esv=b8996be4c462e1ec&sca_upv=1&sxsrf=ADLYWIIOYfvvXReBwwOglg2JYmM2nnbGNg:1718221342891&q=sistema+agil&udm=2&fbs=AEQNm0AuaLfhdrtx2b9ODfK0pnmi046uB92frSWoVskpBryHTrdWqiVbaH6EqK0Fq9hkAkqRDuhGs7UQnPtZiL0Bzcj7hNiQA6b3x6kvZKB_-p3B95ahT-h5-MykP0lSHlmWLWEhIZqraUoqRMWHihIfVJcFNKJTdqy-ev4LluuE9C8RD82dCcKiTa7ebhrR_ra0hsHdTdigc9fJuwfDL1SsvTmCtWpFxw&sa=X&ved=2ahUKEwib_qam6daGAxUVppUCHaiZAOcQtKgLegQIEhAB#vhid=dNwQKCGb9gCybM&vssid=mosaic")
