9f5260baa34d2a9f3dffde840c5af04293b20cb7
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-12 16:57:14 -0300

    Atualizar o index.py

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 import streamlit as st
 st.markdown("Sistema Ágil")
 st.write("---")
-st.image("
-https://photos.app.goo.gl/x3TthWSAEGKsf4yz5")
+st.image("https://photos.app.goo.gl/x3TthWSAEGKsf4yz5")
