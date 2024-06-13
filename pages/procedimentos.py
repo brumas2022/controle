36210a83c84eb5c610069eccae04b66523637558
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-13 00:19:28 -0300

    Update procedimentos.py

--- pages/procedimentos.py
+++ pages/procedimentos.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config("Procedimentos", layout="wide")
 st.markdown("Atualização dos procedimentos")
 st.write("---")
 st.markdown("Lider : Maria da Conceiça Gois")
