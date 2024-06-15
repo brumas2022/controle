7c86ce536e31ddc1a378330a34d4ed8544750460
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-15 00:01:19 -0300

    Update procedimentos.py

--- pages/procedimentos.py
+++ pages/procedimentos.py
@@ -1,7 +1,12 @@
 import streamlit as st
 st.set_page_config("Procedimentos", layout="wide")
-st.markdown("Atualização dos procedimentos")
-st.write("---")
-st.markdown("Lider : Maria da Conceição Gois")
-st.write("---")
-st.markdown("Equipe:")
+lider=st.sidebar.button("Lider")
+equipe=st.sidebar.button("Equipe")
+reunioes=st.sidebar.button("Reunioes")
+objetivos=st.sidebar.button("Objetivos")
+if lider:
+   st.markdown("Maria da Conceição Gois")
+   st.image("flocos.jpg")
+if equipe:
+   st.markdown("Equipe")
+   st.image("bob.jpg")
