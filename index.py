be4d9b37099d4f16053f0c2bfee52fa1db63d2ac
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-14 23:35:25 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.markdown("CONSULTORA DORIS GARISTO LINS - SISTEMA AGIL - SANEAR")
 st.write("---")
 st.image("crie_imagem_de_pessoas_uniformizadas_de_azul_escuro_assistindo_palestra_em_sala_de_reuniao_com_datashow_png.png")
+st.sidebar.button("oi")
