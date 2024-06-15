4615ee22849344592a1f819f3819564fb49fbd0e
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-06-14 23:58:37 -0300

    Update pmsb.py

--- pages/pmsb.py
+++ pages/pmsb.py
@@ -1,15 +1,12 @@
 import streamlit as st
 st.set_page_config("PMSB", layout="wide")
-st.markdown("Atualização e sistematização do PMSB")
-st.write("---")
-st.markdown("Lider : Marcos Brumatti")
-st.write("---")
-st.markdown("Equipe:")
 lider=st.sidebar.button("Lider")
 equipe=st.sidebar.button("Equipe")
 reunioes=st.sidebar.button("Reunioes")
 objetivos=st.sidebar.button("Objetivos")
 if lider:
+   st.markdown("Marcos Brumatti")
    st.image("flocos.jpg")
 if equipe:
-   st.image("bob.jpb")
+   st.markdown("Equipe")
+   st.image("bob.jpg")
