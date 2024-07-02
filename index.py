f0e88cb93617dd4bdab2bd3bcbeb2a18b50f7ce8
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 22:20:55 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config("Projeto Modernização", layout="wide")
 st.markdown("*****PROJETO MODERNIZAÇÃO - SANEAR*****")
 st.write("---")
-st.image("doris.png")
+st.image("doris.PNG")
 consultora=st.sidebar.button("CONSULTORA")
 if consultora:
     st.write("Aqui teremos a foto da consultora")
