7fb818caa843fd4199eb0784a9c3a930f0f40d1f
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 22:08:11 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,5 +2,7 @@ import streamlit as st
 st.markdown("*****PROJETO MODERNIZAÇÃO - SANEAR*****")
 st.write("---")
 st.image("crie_imagem_de_pessoas_uniformizadas_de_azul_escuro_assistindo_palestra_em_sala_de_reuniao_com_datashow_png.png")
-st.sidebar.button("CONSULTORA")
+consultora=st.sidebar.button("CONSULTORA")
+if consultora:
+    st.write("Aqui teremos a foto da consultora")
 
