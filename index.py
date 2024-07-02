c5bb453e5229e09f4a31c668bd3e2cb036986c8c
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 22:10:29 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config("Projeto Modernização", layout="wide")
 st.markdown("*****PROJETO MODERNIZAÇÃO - SANEAR*****")
 st.write("---")
 st.image("crie_imagem_de_pessoas_uniformizadas_de_azul_escuro_assistindo_palestra_em_sala_de_reuniao_com_datashow_png.png")
