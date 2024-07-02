53e96bc745321ca1bdf1507b21a514b8cdc1f7ad
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 22:43:10 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config("Projeto Modernização", layout="wide")
-st.markdown("*****PROJETO MODERNIZAÇÃO - SANEAR*****")
-st.write("---")
+st.header(":green[Sebo do Marcão]", divider="orange")
+st.markdown("Aqui será possivel encontrar coisas com nexo e também totalmente sem nexo :musical_note:")
 st.image("doris.PNG")
 consultora=st.sidebar.button("CONSULTORA")
 if consultora:
