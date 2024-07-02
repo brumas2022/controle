bd59d89afd2a10f4fe623c5cc43deb81012550e9
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-01 22:40:57 -0300

    Update procedimentos.py

--- pages/procedimentos.py
+++ pages/procedimentos.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from st_pages import show_pages_from_config, add_page_title
+
 st.set_page_config("Procedimentos", layout="wide")
 lider=st.sidebar.button("Lider")
 equipe=st.sidebar.button("Equipe")
