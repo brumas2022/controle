e73b93c5698387bea7a3178f000dc83082f38a59
brumas2022 <142266554+brumas2022@users.noreply.github.com>
2024-07-11 16:55:02 -0400

    Update regulamento.py

--- pages/regulamento.py
+++ pages/regulamento.py
@@ -6,7 +6,7 @@ equipe=st.sidebar.button("Equipe")
 reunioes=st.sidebar.button("Reunioes")
 objetivos=st.sidebar.button("Objetivos")
 if lider:
-   st.header(":green[Simone Cavalcanti / Daniel Lobo]", divider="orange")
+   st.header(":green[Simone Cavalcante / Daniel Lobo]", divider="orange")
    col = st.columns((1,1,1))
    col[0].image("simone.PNG")
    col[1].image("daniel.PNG")
